a97b8464011d256f5a86a48e9d8043c17e53dd07
vzhukov <vzhukov@protonmail.ch>
2021-04-22 18:55:02 +0500

    init

---
+++ chess/chessApp.swift
@@ -0,0 +1,17 @@
+//
+//  chessApp.swift
+//  chess
+//
+//  Created by Владислав Жуков on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct chessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BoardView()
+        }
+    }
+}
